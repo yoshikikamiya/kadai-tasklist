4b20a9c7c87951be3cdcf457e8e21650590fd386
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-07-05 08:13:07 +0000

    task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_074413) do
+ActiveRecord::Schema.define(version: 2019_07_05_080501) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
   end
 
 end
