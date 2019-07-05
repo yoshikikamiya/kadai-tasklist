df6ce2369e876ec936efc1f13707e18cf427266a
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-07-05 08:56:03 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show,:edit,:update,:destroy]
     
   def index
       @tasks=Task.all
   end
 
   def show
-      @task = Task.find(params[:id])
   end
 
   def new
@@ -25,11 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
   end
 
   def update
-      @task = Task.find(params[:id])
       
       if @task.update(task_params)
           flash[:success]='タスクは正常に更新されました。'
@@ -41,11 +39,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @task = Task.find(params[:id])
       @task.destroy
       
       flash[:success]='タスクは正常に削除されました。'
       redirect_to task_url
   end
 
+end
+
+
+private
+
+def set_task
+  @task = Task.find(params[:id])
+end
+
+def task_params
+  params.require(:task).permit(:content)
 end
