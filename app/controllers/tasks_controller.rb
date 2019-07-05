cf1bf4d6b04b8a7f6057d75fe70f9be3b1e45f39
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-07-05 12:31:40 +0000

    CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only:[:show,:edit,:update,:destroy]
     
   def index
       @tasks=Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -25,36 +25,33 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
-      
+      @task = Task.find(params[:id])
       if @task.update(task_params)
           flash[:success]='タスクは正常に更新されました。'
           redirect_to @task
       else
           flash.now[:danger]='タスクは更新されませんでした。'
-          reder :edit
+          render :edit
       end
   end
 
   def destroy
+      @task = Task.find(params[:id])
       @task.destroy
       
       flash[:success]='タスクは正常に削除されました。'
-      redirect_to task_url
+      redirect_to tasks_url
   end
-  
-  def task_params
-      params.require(:task).permit(:content)
-  end
-
 end
 
 
 private
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  def task_params
+      params.require(:task).permit(:content,:status)
+  end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    validates:content,presence: true, length:{macimum:255}
-    validates :status,presence: true, length:{maximum:255}
+    validates:content,presence: true, length:{maximum:255}
+    validates :status,presence: true, length:{maximum:10}
 end
