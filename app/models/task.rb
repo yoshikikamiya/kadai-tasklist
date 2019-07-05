04b29c0eba4d8e100888d0b1dffb18fc621a75d9
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-07-05 07:56:13 +0000

    add pagination

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,2 @@
 class Task < ApplicationRecord
-    belongs_to :user
-    validates :content,presence:true,length:{maximum:255}
-    validates :status,presence: true,length:{maximum:10}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_141557) do
+ActiveRecord::Schema.define(version: 2019_07_05_074413) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
     t.string "status"
-    t.string "user"
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
