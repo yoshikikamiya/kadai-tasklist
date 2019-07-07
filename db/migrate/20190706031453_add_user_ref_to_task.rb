978ac0aa8267153a34b94d9d33bb329de42f5a8a
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-07-07 07:45:05 +0000

    migrate修正

--- db/migrate/20190706031453_add_user_ref_to_task.rb
+++ db/migrate/20190706031453_add_user_ref_to_task.rb
@@ -1,10 +1,5 @@
 class AddUserRefToTask < ActiveRecord::Migration[5.2]
   def change
-    create_table :tasks do |t|
-      t.string :content
-      t.references :user,foreign_key: true
-      
-      t.timestamps
-  end
+    add_reference :tasks, :user, foreign_key: true
   end
 end
