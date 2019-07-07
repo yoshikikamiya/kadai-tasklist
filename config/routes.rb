c2ea444785d0d247c0a91e05b5eb578ea835f253
yoshiki.kamiya <kamiya0124510@gmail.com>
2019-07-07 02:29:39 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
     get 'signup', to: 'users#new'
    
     
-    resources :users,only:[:create,:destroy,:show]
+    resources :users,only:[:index, :show, :new, :create]
+    resources :tasks,only:[:create,:destroy]
 end
